77092fc7eb2e81b8d29766861afd8e8684ef9e8d
Chathura Devinda Gamage <105056329+Dewchathu@users.noreply.github.com>
2024-07-03 19:45:14 +0530

    202407031945

--- predict_modified.py
+++ predict_modified.py
@@ -25,7 +25,7 @@ def perform_ocr_on_image(img, coordinates):
     for res in results:
         if len(results) == 1 or (len(res[1]) > 6 and res[2] > 0.2):
             text = res[1]
-
+    print(str(text))
     return str(text)
 
 
